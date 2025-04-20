f511f486d2bfbeea3d1e01abdbf5f53aabd0bd97
unknown <anikmitraxd@gmail.com>
2025-04-20 20:44:28 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import DashBoardButton from "./DashBoardButton";
+
+function Navbar() {
+  return (
+    <nav className="border-b">
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        {/* LEFT SIDE -LOGO */}
+        <Link
+          href="/"
+          className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80 transition-opacity"
+        >
+          <span className="bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent
+">
+            //Code_View
+          </span>
+        </Link>
+
+        {/* RIGHT SIDE - ACTIONS */}
+        <SignedIn>
+          <div className="flex items-center space-x-4 ml-auto">
+            <DashBoardButton/>
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
