a2afae461a73819d95af9dea915a64ab42595733
unknown <anikmitraxd@gmail.com>
2025-05-06 07:27:51 +0600

    Pre-release commit 6

--- src/app/(root)/interviews/[id]/page.tsx
+++ src/app/(root)/interviews/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { InterviewComments } from "@/components/InterviewComments";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
